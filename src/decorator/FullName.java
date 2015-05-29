f1236cf6a3a064359ceba405fcb4c34c5699b117
Jun Chen <tlnd.sky@gmail.com>
2015-05-29 23:11:25 +1000

    Example Exam Questions: slides14 decorator.

--- src/decorator/FullName.java
+++ src/decorator/FullName.java
@@ -1,21 +1,35 @@
 package decorator;
-
+/**
+ * Example Exam Questions: slides14 decorator.
+ * @author Jason
+ *
+ */
 public class FullName extends Name {
 	String surname;
+	
+	Name name;
 
-	public FullName(String firstname, String surname) {
+	/*public FullName(String firstname, String surname) {
 		this.name = firstname;
 		this.surname = surname;
-	}
+	}*/
 	
 	// decorator design pattern:
-	
-
-	String showname() {
-		return super.showname() + " " + surname;
+	public FullName(String surname, Name name) {
+		this.surname = surname;
+		this.name = name;
 	}
 
-	public static void main(String[] args) {
+	/*String showname() {
+		return super.showname() + " " + surname;
+	}*/
+	
+	String showname() {
+		return name.showname() + " " + surname;
+	}
 
+	public static void main(String[] args) { 
+		Name name = new FullName("Chen", new Name("Jun"));
+		System.out.println(name.showname());
 	}
 }
